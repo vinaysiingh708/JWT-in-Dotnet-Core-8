276011aff4256ca0103bad9e27f93873a33beb31
vinay singh <vinaysingh708@yahoo.com>
2024-07-21 16:25:28 +0530

    remove unused namespace

--- jwt token/Controllers/AuthController.cs
+++ jwt token/Controllers/AuthController.cs
@@ -1,12 +1,6 @@
 ﻿using jwt_token.Services.Interface;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
+
 
 [ApiController]
 [Route("api/[controller]")]
